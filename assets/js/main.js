3a1d091ed8938cce484ce7f3db3b26a13981b873
OthmaneDevLab <adamlamzaouak987@gmail.com>
2024-03-08 05:29:06 +0100

    Header and Footer

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.fade1').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        infinite: true,
+        autoplay: true,
+        cssEase: 'linear'
+    });
+});
